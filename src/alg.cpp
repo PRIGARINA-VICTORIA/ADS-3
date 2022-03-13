7e50c0bb1d2ff2bd3f61ba8cb1c920d12c907d26
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-03-13 18:47:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,4 +3,19 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
+  int count_value = size + 1;
+  int left = 0;
+  int right = size - 1;
+  int mid = size / 2;
+  for (int i = 0; i < mid; i++) {
+    if (arr[i] != value) {
+      count_value--;
+    }
+  }
+  for (int j = size; j >= mid; j--) {
+    if (arr[j] != value) {
+      count_value--;
+    }
+  }
+  return count_value;
 }
